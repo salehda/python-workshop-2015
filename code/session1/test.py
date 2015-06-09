2989ca34a17f3af9008ab85dd8e7d8682945755f
salehda <mech_Davidashraf@yahoo.com>
2015-06-09 09:11:34 -0400

    Session1 my code

---
+++ code/session1/test.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 08 11:04:33 2015
+
+@author: David
+"""
+
+print"David"
+for i in range(5):
+    print i
+    
+def flipstring(str):
+    """
+    create a function to flip a string
+    
+    """
+mystr_list=list(str)
+reverselist=list.reverse(mystr_list)
+final="".join(mystr_list)
+print final
+    
+def fortyfunc(num)
+    
+    
---
+++ code/session1/test2.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 08 11:45:52 2015
+
+@author: David
+"""
+
